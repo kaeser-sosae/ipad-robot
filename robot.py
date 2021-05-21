77db655b8c1eec015c00b6bab698195b0dc2c1ab
Gavin Kennedy <gavin@albanylan.com>
2021-05-21 13:43:07 +1000

    Robot works

--- robot.py
+++ robot.py
@@ -231,8 +231,8 @@ def press_power_button(seconds):
 		dexarm.fast_move_to(get_current_location('x'), 326, get_current_location('z'), 5000)
 		dexarm.fast_move_to(-148, get_current_location('y'), get_current_location('z'), 5000)
 		dexarm.fast_move_to(get_current_location('x'), get_current_location('y'), -106, 5000)
-		dexarm._send_cmd("G4 S5\n")
 		dexarm.fast_move_to(-136, get_current_location('y'), get_current_location('z'), 5000)
+		dexarm._send_cmd("G4 S" + seconds + "\n")
 		dexarm.fast_move_to(-148, get_current_location('y'), get_current_location('z'), 5000)
 		dexarm.fast_move_to(get_current_location('x'), get_current_location('y'), -20, 5000)
 
