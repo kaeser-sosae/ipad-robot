91eae554da7e9062f43f97a21f34a23dc048fd11
Gavin Kennedy <gavin@albanylan.com>
2021-05-21 12:44:48 +1000

    Initial commit

--- go_home.py
+++ go_home.py
@@ -3,17 +3,11 @@ from pydexarm import Dexarm
 
 dexarm = Dexarm("/dev/ttyACM0")
 
-x = sys.argv[1]
-y = sys.argv[2]
-z = sys.argv[3]
-
 print('Current pos = ' + str(dexarm.get_current_position()))
 print('Encoder pos = ' + str(dexarm.get_encoder_position()))
 print(' ')
 dexarm._send_cmd("M1112")
 time.sleep(10)
-print('Moved to X' + x + ' Y' + y + ' Z' + z)
-print(' ')
 print('New Current pos = ' + str(dexarm.get_current_position()))
 print('New Encoder pos = ' + str(dexarm.get_encoder_position()))
 
